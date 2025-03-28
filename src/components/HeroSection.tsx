1e5bf969d2dbf089c72868c84d7e9a654707a5a7
YungSpielburg <126429031+YungSpielburg@users.noreply.github.com>
2025-03-27 19:24:35 -0700

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -15,7 +15,7 @@ const podcasts = [
   { 
     name: 'Acquired.fm', 
     description: 'Custom intro',
-    audioFile: '/who-got-the-truth.mp3',
+    audioFile: '/who-got-the-truth-fixed.mp3',
     image: '/acquired-podcast-jpg.jpeg',
     hideText: true,
     buttonPosition: 'bottom' as const
@@ -31,7 +31,7 @@ const podcasts = [
   { 
     name: 'Not Boring', 
     description: 'Outro theme',
-    audioFile: '/not-boring-outro-theme.mp3',
+    audioFile: '/not-boring-fix.mp3',
     image: '/not-boring-image.jpg',
     hideText: true,
     buttonPosition: 'bottom' as const
